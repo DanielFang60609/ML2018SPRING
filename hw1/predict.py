284230bda8073c5f015cc94cedd120b4c5e11e61
DanielFang60609 <da60609@yahoo.com.tw>
2018-03-22 23:26:05 +0900

    Update and rename predictF.py to predict.py

--- hw1/predict.py
+++ hw1/predict.py
@@ -5,15 +5,15 @@ import sys
 import os
 
 theta=np.load('model3HR_X2_norm_SelectMonth_Bias.npy')
-print(theta)
-print(type(theta))
+#print(theta)
+#print(type(theta))
 bias=theta[108]
 theta=np.delete(theta,108,0)
 
 #theta=theta.reshape(18,6)
-print(theta)
-print(theta.shape)
-print(bias,'bias')
+#print(theta)
+#print(theta.shape)
+#print(bias,'bias')
 
 def NormMatrix():
     NM=[]
@@ -54,9 +54,9 @@ def NormMatrix():
         NM.append(5)
     for i in range(ran):
         NM.append(3)
-    print(NM,'NM=?')
+ #   print(NM,'NM=?')
     NM=np.asarray(NM)
-    print(NM.shape)
+  #  print(NM.shape)
     return NM
 
 #input='./test.csv'
@@ -66,15 +66,15 @@ data = pd.read_csv(input,encoding = "ANSI",header=None)
 #selectCol=[0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
 data=data.iloc[:,8:11]
 data=pd.DataFrame(data=data.values)
-print(data)
-print(type(data))
-print(data.shape)
+#print(data)
+#print(type(data))
+#print(data.shape)
 for i in range(4680):
     for k in range(3):
         if data.values[i][k] == "NR":
             data.values[i][k]=0
         data.values[i][k]=float(data.values[i][k])
-print(data)
+#print(data)
 ans=[]
 NormM=NormMatrix()
 for i in range(260):
@@ -92,7 +92,7 @@ for i in range(260):
 #    print(i,'i')
 #    print(pm25,'pm25')
     ans.append(pm25)
-print(ans)
+#print(ans)
 col = ['id', 'value']
 #path="./ans1D3HR_X2_norm_SelectMonth_Bias_ISTHISCORRECT.csv"
 path=sys.argv[2]
