f5ecf7686d042c000f11b18c4757094b0c2954a7
DanielFang60609 <da60609@yahoo.com.tw>
2018-04-19 23:45:34 +0900

    Update hw3_test.sh

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,3 @@
 # using TA's provided features
-wget 'https://www.dropbox.com/s/88aulayxui7sy8a/_bestModel?dl=1'
+wget 'https://www.dropbox.com/s/88aulayxui7sy8a/_bestModel?dl=1' -o bestmodel
 python predict.py $1 $2 $3
