a9ee60c1c420a7ff182fda9e878c028d03744570
DanielFang60609 <da60609@yahoo.com.tw>
2018-06-07 23:16:55 +0800

    Update hw5_test.sh

--- hw5/hw5_test.sh
+++ hw5/hw5_test.sh
@@ -1,2 +1,2 @@
-wget 'https://www.dropbox.com/s/d6oc3gcjusav8sd/model.0002-0.9961.h5?dl=0' -O bestModel
+wget 'https://www.dropbox.com/s/d6oc3gcjusav8sd/model.0002-0.9961.h5?dl=0' -O bestModel.h5
 python predict.py $1 $2 $3
