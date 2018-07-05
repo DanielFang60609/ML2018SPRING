99efcba3a77e6ce1ed5f1c54eba7ee8eee7fc811
DanielFang60609 <da60609@yahoo.com.tw>
2018-07-06 01:34:03 +0800

    Add files via upload

---
+++ final/download_dataset.sh
@@ -0,0 +1,4 @@
+python ./src/download.py 1KPUkX92vBtDc8LJPrUMDYxUF3AP_Wnov audio_train.zip
+python ./src/download.py 1NiaaUNKytgDHkZaPNDGGdIvs7W5WSnDR audio_test.zip
+unzip audio_train.zip -d ./audio_train
+unzip audio_test.zip -d ./audio_test/audio_test
