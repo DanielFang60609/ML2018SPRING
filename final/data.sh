581bd147e2cfdab882f9d6bde66e0f0567b6aed0
DanielFang60609 <da60609@yahoo.com.tw>
2018-07-06 01:20:00 +0800

    Add files via upload

--- final/data.sh
+++ final/data.sh
@@ -1,22 +1,3 @@
-wget "https://www.dropbox.com/s/ce8i6b0tuek9koo/0.npy?dl=0" -O 0.npy
-wget "https://www.dropbox.com/s/i1who7oem9hqi19/1.npy?dl=0" -O 1.npy
-wget "https://www.dropbox.com/s/i3ir0o3fv3nk0t2/2.npy?dl=0" -O 2.npy
-wget "https://www.dropbox.com/s/wmfqsh9jpyqrp6z/3.npy?dl=0" -O 3.npy
-wget "https://www.dropbox.com/s/qpdwqpz3y44bwyk/4.npy?dl=0" -O 4.npy
-wget "https://www.dropbox.com/s/lu68cjpbny9psgg/5.npy?dl=0" -O 5.npy
-wget "https://www.dropbox.com/s/5zou3fjqu5v8ue2/6.npy?dl=0" -O 6.npy
-wget "https://www.dropbox.com/s/jpeu6nhyxu3dw3l/7.npy?dl=0" -O 7.npy
-wget "https://www.dropbox.com/s/4g9lmzs2pmgbhpr/8.npy?dl=0" -O 8.npy
-wget "https://www.dropbox.com/s/bjadimxg7bb6dzi/9.npy?dl=0" -O 9.npy
-wget "https://www.dropbox.com/s/8s0oswfb59ajlv7/10.npy?dl=0" -O 10.npy
-wget "https://www.dropbox.com/s/5gmvdkazhkebkd0/11.npy?dl=0" -O 11.npy
-wget "https://www.dropbox.com/s/g2mcfrsl5w31toa/12.npy?dl=0" -O 12.npy
-wget "https://www.dropbox.com/s/2bx2yr1s6oxfegi/13.npy?dl=0" -O 13.npy
-wget "https://www.dropbox.com/s/7zupbgwn5ot1s7u/14.npy?dl=0" -O 14.npy
-wget "https://www.dropbox.com/s/xmmo7enbmc8kpzl/15.npy?dl=0" -O 15.npy
-wget "https://www.dropbox.com/s/7podtewno84bees/16.npy?dl=0" -O 16.npy
-wget "https://www.dropbox.com/s/8ir1kygmuag72n2/17.npy?dl=0" -O 17.npy
-wget 'https://drive.google.com/uc?authuser=0&id=1o2D0bQK_P150UubQ_vej9Hgso7zREs1Y&export=download' -O dataID.npy
-python ./src/predict.py 
-
-
+python ./src/download.py 1EdkwAn2Xd2EVsMi9Nxn9QkAY39LGOuTL train.csv
+python ./src/download.py 1tRFGD0A3fCaC7AbEDR-dQTe9Aga4RTZy sr44100hops160bands64fft_fmin_fmax400_20_16000label512repeatData.npy
+python ./src/download.py 1o_FH4GGu8wQgiqEm1W2UOeT3TLhZR2yH sr44100hops160bands64fft_fmin_fmax400_20_16000mel512repeatData.npy
